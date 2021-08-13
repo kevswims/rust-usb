0315f81756b2367352ace191e472723f535653c2
Kevin Lannen <kevin.lannen@gmail.com>
2021-08-13 14:35:52 -0600

    Setup an LED for debugging

--- src/bin/usb_serial.rs
+++ src/bin/usb_serial.rs
@@ -4,6 +4,7 @@
 use rust_usb as _; // global logger + panicking-behavior + memory layout
 
 use stm32f4xx_hal::{prelude::*, stm32};
+use cortex_m::asm::nop;
 
 #[cortex_m_rt::entry]
 fn main() -> ! {
@@ -19,9 +20,16 @@ fn main() -> ! {
         .pclk2(24.mhz())
         .freeze();
 
-    
+    // Configure the on-board RED LED (PB14)
+    let gpiob = dp.GPIOB.split();
+    let mut led = gpiob.pb14.into_push_pull_output();
+
+    // Turn the LED off
+    led.set_low().unwrap();
 
     defmt::info!("Hello, world!");
 
-    rust_usb::exit()
+    loop {
+        nop();
+    }
 }
