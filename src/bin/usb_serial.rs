7a8683e011a7a76a8f79dfdaff8340ed9f6d29e9
Kevin Lannen <kevin.lannen@gmail.com>
2021-08-15 21:17:18 -0600

    In the middle of doing stuff

--- src/bin/usb_serial.rs
+++ src/bin/usb_serial.rs
@@ -8,7 +8,7 @@ use stm32f4xx_hal::{
     prelude::*,
     stm32,
 };
-use usb_device::prelude::*;
+use usb_device::{class::ControlOut, prelude::*};
 use usbd_serial::{SerialPort, USB_CLASS_CDC};
 
 static mut EP_MEMORY: [u32; 1024] = [0; 1024];
@@ -50,48 +50,73 @@ fn main() -> ! {
 
     let usb_bus = UsbBus::new(usb, unsafe { &mut EP_MEMORY });
 
-    let mut serial = SerialPort::new(&usb_bus);
+    // let mut serial = SerialPort::new(&usb_bus);
 
     let mut usb_dev = UsbDeviceBuilder::new(&usb_bus, UsbVidPid(0x16c0, 0x27dd))
         .manufacturer("Fake Company")
         .product("Serial Port")
         .serial_number("TEST")
-        .device_class(USB_CLASS_CDC)
+        .device_class(0xFF)
         .build();
 
+    let mut custom_class = CustomClass {};
+
     loop {
-        if !usb_dev.poll(&mut [&mut serial]) {
+        if !usb_dev.poll(&mut [&mut custom_class]) {
             continue;
         }
 
-        let mut buffer = [0u8; 64];
+        // let mut buffer = [0u8; 64];
 
-        match serial.read(&mut buffer) {
-            Ok(count) if count > 0 => {
-                // Turn on the LED
-                led.set_high().unwrap();
+        // match serial.read(&mut buffer) {
+        //     Ok(count) if count > 0 => {
+        //         // Turn on the LED
+        //         led.set_high().unwrap();
 
-                // Echo back in upper case
-                for c in buffer[0..count].iter_mut() {
-                    if 0x61 <= *c && *c <= 0x7a {
-                        *c &= !0x20;
-                    }
-                }
+        //         // Echo back in upper case
+        //         for c in buffer[0..count].iter_mut() {
+        //             if 0x61 <= *c && *c <= 0x7a {
+        //                 *c &= !0x20;
+        //             }
+        //         }
 
-                let mut write_offset = 0;
-                while write_offset < count {
-                    match serial.write(&buffer[write_offset..count]) {
-                        Ok(len) if len > 0 => {
-                            write_offset += len;
-                        }
-                        _ => {}
-                    }
-                }
-            }
-            _ => {}
-        }
+        //         let mut write_offset = 0;
+        //         while write_offset < count {
+        //             match serial.write(&buffer[write_offset..count]) {
+        //                 Ok(len) if len > 0 => {
+        //                     write_offset += len;
+        //                 }
+        //                 _ => {}
+        //             }
+        //         }
+        //     }
+        //     _ => {}
+        // }
 
         // Turn off the LED
         led.set_low().unwrap();
     }
+}
+
+struct CustomClass {
+
+}
+
+use usb_device::class_prelude::*;
+
+impl<B: usb_device::bus::UsbBus> usb_device::class::UsbClass<B> for CustomClass {
+
+    fn control_out(&mut self, xfer: ControlOut<B>) {
+        let req = *xfer.request();
+
+        defmt::info!("Hello from control transfer");
+        if !(req.request_type == control::RequestType::Vendor && req.recipient == control::Recipient::Device)
+        {
+            return;
+        }
+    }
+
+    // fn get_configuration_descriptors(&self, writer: &mut DescriptorWriter) -> Result<()> {
+    //     writer.interface()
+    // }
 }
